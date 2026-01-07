9c6a0efae986dcc383b43de870b7f89dfca4b07c
Diego3143 <154592219+Diego3143@users.noreply.github.com>
2026-01-07 01:39:19 -0500

    Add files via upload

--- src/components/comments/CommentModal.tsx
+++ src/components/comments/CommentModal.tsx
@@ -3,8 +3,9 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
-import Filter from 'bad-words';
+import BadWords from "bad-words";
 
+const filter = new BadWords();
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogDescription } from '@/components/ui/dialog';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
